bc791ed5adcc0d98232809c2689d204c226c9621
KritAsawaniramol <krit.as@ku.th>
2025-01-24 20:43:39 +0700

    move logic to handle function

--- index.js
+++ index.js
@@ -20,22 +20,21 @@ const cardNumbers = [
 let playerChips = 0;
 
 class Deck {
-    constructor() {
-        this.cards = createDeck();
-    }
+  constructor() {
+    this.cards = createDeck();
+  }
 
-    // Shuffle using Fisher-Yates Shuffle
-    shuffleDeck() {
-        for (var i = this.cards.length - 1; i > 0; i--) {
+  // Shuffle using Fisher-Yates Shuffle
+  shuffleDeck() {
+    for (var i = this.cards.length - 1; i > 0; i--) {
+      // Returns a random integer from 0 to this.cards.length - 1
+      const randomIndex = Math.floor(Math.random() * (i + 1));
 
-            // Returns a random integer from 0 to this.cards.length - 1
-            const randomIndex = Math.floor(Math.random() * (i + 1));
-        
-            var temp = this.cards[i];
-            this.cards[i] = this.cards[randomIndex];
-            this.cards[randomIndex] = temp;
-          }
+      var temp = this.cards[i];
+      this.cards[i] = this.cards[randomIndex];
+      this.cards[randomIndex] = temp;
     }
+  }
 }
 
 function createDeck() {
@@ -54,62 +53,72 @@ function createDeck() {
 let deck = new Deck();
 
 const isValidBet = (bet) => {
-    if (isNaN(bet)) {
-        console.log("Please enter a valid number");
-        return false;
-    }
-    if (bet < 0) {
-        console.log("Please enter a positive number ( >= 0 )");
-        return false;
-    }
-    return true;
-}
+  if (isNaN(bet)) {
+    console.log("Please enter a valid number");
+    return false;
+  }
+  if (bet < 0) {
+    console.log("Please enter a positive number ( >= 0 )");
+    return false;
+  }
+  return true;
+};
 
 const playGame = () => {
-    rl.question("Please put your bet:\n", (answer) => {
-        if (!isValidBet(answer)) {
-            playGame();
-            return;
-        }
-        let bet = parseInt(answer);
+  rl.question("Please put your bet:\n", (answer) => {
+    handlePlayGameInput(answer);
+  });
+};
 
-        deck.shuffleDeck();
+const handlePlayGameInput = (answer) => {
+  if (!isValidBet(answer)) {
+    playGame();
+    return;
+  }
+  let bet = parseInt(answer);
 
-        let playerCard = [deck.cards[0], deck.cards[1]];
-        let dealerCard = [deck.cards[2], deck.cards[3]];
+  deck.shuffleDeck();
 
-        console.log("You got " + playerCard[0].card + ", " + playerCard[1].card);
-        console.log("The dealer got " + dealerCard[0].card + ", " + dealerCard[1].card);
-          
-        let playerScore = playerCard[0].value + playerCard[1].value;
-        let dealerScore = dealerCard[0].value + dealerCard[1].value;
-      
-        if (playerScore > dealerScore) {
-          playerChips += bet;
-          process.stdout.write("You won!!, recived: " + bet + " chips\n");
-        } else if (playerScore < dealerScore) {
-          playerChips -= bet;
-          process.stdout.write("You lost!!, forfeited: " + bet + " chips\n");
-        } else {
-          process.stdout.write("You tie!!\n");
-        }
-        continueGame();
-      });
+  let playerCard = [deck.cards[0], deck.cards[1]];
+  let dealerCard = [deck.cards[2], deck.cards[3]];
+
+  console.log("You got " + playerCard[0].card + ", " + playerCard[1].card);
+  console.log(
+    "The dealer got " + dealerCard[0].card + ", " + dealerCard[1].card
+  );
+
+  let playerScore = playerCard[0].value + playerCard[1].value;
+  let dealerScore = dealerCard[0].value + dealerCard[1].value;
+
+  if (playerScore > dealerScore) {
+    playerChips += bet;
+    process.stdout.write("You won!!, recived: " + bet + " chips\n");
+  } else if (playerScore < dealerScore) {
+    playerChips -= bet;
+    process.stdout.write("You lost!!, forfeited: " + bet + " chips\n");
+  } else {
+    process.stdout.write("You tie!!\n");
+  }
+  continueGame();
 };
 
 const continueGame = () => {
-    rl.question("Wanna play more (Yes/No)?\n", (answer) => {
-        answer = answer.toLowerCase();
-        if (answer === "yes") {
-            playGame();
-        } else if (answer === "no") {
-            rl.close();
-            return;
-        } else {
-            console.log("Please enter a valid input (Yes/No)");
-            continueGame();
-        }
-    });
+  rl.question("Wanna play more (Yes/No)?\n", (answer) => {
+    handleContinueGamaInput(answer);
+  });
+};
+
+const handleContinueGamaInput = (answer) => {
+  answer = answer.toLowerCase();
+  if (answer === "yes") {
+    playGame();
+  } else if (answer === "no") {
+    rl.close();
+    return;
+  } else {
+    console.log("Please enter a valid input (Yes/No)");
+    continueGame();
+  }
 };
 
 rl.on("close", () => {
@@ -122,3 +131,10 @@ rl.on("close", () => {
 });
 
 playGame();
+
+module.exports = {
+    createDeck,
+    isValidBet,
+    handlePlayGameInput,
+    handleContinueGamaInput,
+}
